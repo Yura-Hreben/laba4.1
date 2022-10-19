cc6c125b1117700749c7ed49260186ced3f2bd72
yrii <yural1rn@gmail.com>
2022-10-19 15:54:18 +0300

    laba4.1

---
+++ laba4.1/laba4.1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int k, N, i;
+	double P;
+	
+	cout << "N = "; cin >> N;
+	P = 15;
+	i = 1;
+	while (i <= N)
+	{
+		P *= (sin(i) * sin(i) + cos(1 / i) * cos(1 / i)) / i * i;
+		i++;
+	}
+	cout << P << endl;
+	
+	P = 15;
+	i = 1;
+	do {
+		P *= (sin(i) * sin(i) + cos(1 / i) * cos(1 / i)) / i * i;
+		i++;
+	} while (i <= N);
+	cout << P << endl;
+	P = 15;
+	for (i = 1; i <= N; i++)
+	{
+		P *= (sin(i) * sin(i) + cos(1 / i) * cos(1 / i)) / i * i;
+	}
+	cout << P << endl;
+	P = 15;
+	for (i = N; i >= 1; i--)
+	{
+		P *= (sin(i) * sin(i) + cos(1 / i) * cos(1 / i)) / i * i;
+	}
+	cout << P << endl;
+	return 0;
+}
